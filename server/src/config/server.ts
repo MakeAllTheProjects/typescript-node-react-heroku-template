33119c10b2c4367d23e75cffc30de0d6cbc3949d
Meghan Bomberger <meghanbomberger@gmail.com>
2021-03-05 19:38:15 -0700

    heroku additions

--- server/src/config/server.ts
+++ server/src/config/server.ts
@@ -1,14 +1,47 @@
+require('dotenv').config()
+
 import cors from 'cors'
 import express from 'express'
+import path from 'path'
+import serveStatic from 'serve-static'
 
 const app = express()
 
-const PORT: string | number = process.env.PORT || 5000
+const whitelist = [
+  'localhost:3000',
+  `localhost:${process.env.PORT || 5000}`,
+  process.env.HEROKU_URL
+]
 
-app.use(cors())
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("** Origin of request " + origin.headers.host)
+    if (whitelist.indexOf(origin.headers.host) !== -1 || !origin) {
+      console.log("Origin acceptable")
+      callback(null, true)
+    } else {
+      console.log("Origin rejected")
+      callback(new Error('Not allowed by CORS - ' + origin.headers.host))
+    }
+  }
+}
+
+app.use(express.json())
+app.use(cors(corsOptions.origin))
+app.use(serveStatic(__dirname + '/client/build'))
 
 app.get("/api/hello", (req, res) => {
   res.send({ message: "Hello World!" })
 })
 
-app.listen(PORT, () => console.log(`hosting @${PORT}`))
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')))
+  app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+  });
+}
+
+app.listen(
+  process.env.PORT || 4000,
+  () => console.log(`Server listening on port ${process.env.PORT || 4000}`)
+)
